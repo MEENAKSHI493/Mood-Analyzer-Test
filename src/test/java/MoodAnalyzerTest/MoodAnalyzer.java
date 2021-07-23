50d4276ac8a145057753182c6b2eb73bbe1d3056
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-24 00:04:30 +0530

    Respond Hoppy or Sad

--- src/test/java/MoodAnalyzerTest/MoodAnalyzer.java
+++ src/test/java/MoodAnalyzerTest/MoodAnalyzer.java
@@ -1,28 +1,14 @@
 package MoodAnalyzerTest;
 
 public class MoodAnalyzer {
-	 private String message;
 
-	    
-	    public MoodAnalyzer(String message) {
-	        this.message = message;
-	    }
+	public String analyseMood(String message) {
 
-	 // Check Sad or Happy Mood
-		public String analyseMood() {
-			try {
-				if (message.toLowerCase().contains("sad"))
-					return "SAD";
+		if (message.toLowerCase().contains("sad"))
+			return "SAD";
 
-				return "HAPPY";
-			} 
-			catch (NullPointerException e) {
-				throw new NullPointerException("Invalid Name");
+		return "HAPPY";
 
-			}
-
-		}
 	}
-	
 
-
+}
