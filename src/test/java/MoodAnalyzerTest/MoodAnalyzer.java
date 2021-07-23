10d92ef43e6d26633c59183a424eb32142ec5e26
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-23 23:54:26 +0530

    Exception Handaling

---
+++ src/test/java/MoodAnalyzerTest/MoodAnalyzer.java
@@ -0,0 +1,28 @@
+package MoodAnalyzerTest;
+
+public class MoodAnalyzer {
+	 private String message;
+
+	    
+	    public MoodAnalyzer(String message) {
+	        this.message = message;
+	    }
+
+	 // Check Sad or Happy Mood
+		public String analyseMood() {
+			try {
+				if (message.toLowerCase().contains("sad"))
+					return "SAD";
+
+				return "HAPPY";
+			} 
+			catch (NullPointerException e) {
+				throw new NullPointerException("Invalid Name");
+
+			}
+
+		}
+	}
+	
+
+
---
+++ src/test/java/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -0,0 +1,43 @@
+package MoodAnalyzerTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+		// Test-Case for SadMood
+	
+		@Test
+		public void givenMessage_whenSad_ShouldReturnSad() {
+			MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Sad");
+			String actual = moodAnalyzer.analyseMood();
+			String expected = "SAD";
+			Assert.assertEquals(expected, actual);
+
+		}
+
+		// Test-Case for HappyMood
+		
+		@Test
+		public void givenMessage_whenHappy_ShouldReturnHappy() {
+			MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am Happy");
+			String actual = moodAnalyzer.analyseMood();
+			String expected = "HAPPY";
+			Assert.assertEquals(expected, actual);
+
+		}
+		// Test for Null
+
+		@Test
+		public void givenMessage_whenNull_ShouldReturnNull() {
+			String excepted = "Invalid message";
+			try {
+				MoodAnalyzer moodAnalyzer = new MoodAnalyzer("Null");
+				moodAnalyzer.analyseMood();
+			} catch (NullPointerException e) {
+				Assert.assertEquals(excepted, e.getMessage());
+			}
+
+		}
+	
+}
